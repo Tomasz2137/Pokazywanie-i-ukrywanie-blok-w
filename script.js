8c7d7fc8b693153e843d291ee49ae51006fcee46
Tomasz <tomasz.gawlik@zsz.bobowa.pl>
2025-03-25 11:14:49 +0100

    v2

--- script.js
+++
@@ -1,7 +0,0 @@
-function showBlock() {
-    document.getElementById('myBlock').style.display = 'block';
-}
-
-function hideBlock() {
-    document.getElementById('myBlock').style.display = 'none';
-}
